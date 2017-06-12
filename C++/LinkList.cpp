d3667279f4b7e94bc50af9edceaf5c2d20a4c284
Tim Ho <github.timho@gmail.com>
2017-06-11 19:50:08 -0700

    added List.h, LinkList.cpp

--- C++/LinkList.cpp
+++ C++/LinkList.cpp
@@ -1,6 +1,23 @@
+#include <cstdlib>
 #include <iostream>
+#include "List.h"
 using namespace std; 
 
-int main () {
-  cout << "hello" << endl; 
+int main(int argc, char** argv) {
+
+	List node; 
+
+	node.AddNode(3); 
+	node.AddNode(5);
+	node.AddNode(7);
+	node.PrintList(); 
+
+	node.DeleteNode(5);
+	node.DeleteNode(7); 
+	node.AddNode(1); 
+	node.AddNode(2); 
+	node.PrintList(); 
+
+	system("pause");
+	return 0; 
 }
---
+++ C++/List.h
@@ -0,0 +1,86 @@
+#pragma once
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+using namespace std; 
+class List {
+private: 
+	typedef struct node
+	{
+		int data; 
+		node* next; 
+	}* nodePtr; 
+
+	nodePtr head; 
+	nodePtr curr; 
+	nodePtr temp; 
+
+public: 
+	List(); 
+	void AddNode(int addData);
+	void DeleteNode(int delData); 
+	void PrintList(); 
+};
+
+
+List::List() {
+	head = NULL; 
+	curr = NULL; 
+	temp = NULL; 
+}
+
+void List::AddNode(int addData) {
+	nodePtr n = new node;
+	n->next = NULL; 
+	n->data = addData; 
+
+	if (head != NULL) {
+		curr = head; 
+		while (curr->next != NULL)
+		{
+			curr = curr->next; 
+		}
+		curr->next = n; 
+	}
+	else
+	{
+		head = n; 
+	}
+}
+
+void List::DeleteNode(int delData) {
+	nodePtr delN = NULL; 
+	temp = head; 
+	curr = head; 
+
+	while (curr != NULL && curr->data != delData)
+	{
+		temp = curr; 
+		curr = curr->next; 
+	}
+
+	if (curr == NULL)
+	{
+		cout << delData << " Is not in the list." << endl;
+		delete delN; 
+	}
+	else {
+		delN = curr; 
+		curr = curr->next; 
+		temp->next = curr; 
+		delete delN; 
+		cout << "the data " << delData << " Was deleted." << endl; 
+	}
+}
+
+void List::PrintList() {
+	curr = head; 
+	while (curr != NULL) {
+		cout << curr->data << endl; 
+		curr = curr->next; 
+	}
+}
+
+#endif // !1
+
+
